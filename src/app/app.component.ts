9f82a258c551588d329434eb7d2377804a13f930
Bastien nibelumachinchosepoufpouf <bastien-gandalug@hotmail.fr>
2024-02-12 15:42:23 +0100

    add click button

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
 import { Router, RouterOutlet } from '@angular/router';
-
+import {MatButtonToggleModule} from '@angular/material/button-toggle';
+import {MatIconModule} from '@angular/material/icon';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, ReactiveFormsModule],
+  imports: [RouterOutlet, ReactiveFormsModule, MatButtonToggleModule, MatIconModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
@@ -13,8 +14,8 @@ export class AppComponent {
 
   currentAction: any;
   actions = [
-    {route : "/home", title : "Home", icon: "house"},
-    {route : "/gpt", title : "GPT", icon: "person"},
+    {route : "/home", title : "Home", icon: "house", disabled : false},
+    {route : "/gpt", title : "GPT", icon: "person",  disabled : true},
   ]
 
 constructor(private router: Router) {
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,12 +1,25 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-
+import { MatButtonModule } from '@angular/material/button';
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [MatButtonModule, CommonModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
 export class HomeComponent {
+  mouseOver = false;
+  mouseOverCount = 0;
+
+
+  handleMouseOver() {
+    this.mouseOver= true;
+    this.mouseOverCount++;
+  }
+  handleMouseOut() {
+    this.mouseOver = false;
+    this.mouseOverCount = this.mouseOverCount >= 5 ? 0 : this.mouseOverCount;
+  }
 
 }
